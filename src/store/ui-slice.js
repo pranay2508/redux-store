a0a78a357bf0f57d560de24de7a591e573710993
pranay2508 <bhatnagarpranay8@gmail.com>
2023-08-16 22:01:52 +0530

    added notifications

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,11 +2,15 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
   name: 'ui',
-  initialState: { cartIsVisible: false },
+  initialState: { cartIsVisible: false , notification :null},
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state,action){
+      state.notification={status:action}
     }
+
   }
 });
 
